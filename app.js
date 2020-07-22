8931614afe20a39fb9136015366e7e6195a7f5e0
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-07-22 20:38:02 +0200

    dark mode action added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $('#input').click(function() {
-
-    })
-})
+        $(".cont-top").toggleClass("dark-mode-top");
+        $(".cont-middle").toggleClass("dark-mode-middle");
+        $(".card-body").toggleClass("dark-mode-card");
+    });
+});
